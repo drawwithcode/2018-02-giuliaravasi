9015a2bacd2bd4e1abb55af07863385a00f465e8
Giulia <giuliaravasi@MacBook-Pro-di-Giulia.local>
2018-10-02 09:54:45 +0200

    New code

--- sketch.js
+++ sketch.js
@@ -3,9 +3,24 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(60);
+  angleMode(DEGREES);
+  frameRate(60);
 }
 
 function draw() {
-  // put drawing code here
+  push();
+  translate(width/2, height/2);
+  rotate(frameCount*cos(80)*sin(80)+150);
+
+  stroke(255);
+  noFill();
+  stroke(lerpColor(color('#0028ff'), color('#ffd700'), frameCount/1100));
+  line(sin(frameCount)*150, cos(frameCount)*150, sin(0)*150, cos(frameCount)*150);
+  pop();
+
+  if (frameCount == 900) {
+   noLoop();
+    }
 }
